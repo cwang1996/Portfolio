3f5569528eec1fbddf5633502810c6e422690c30
Calvin Wang <calvinwang@yahoo.com>
2021-07-21 13:45:57 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const menuButton = document.querySelector('.menu');
-const navbar = document.querySelector('.navbarlinks');
+const text_auto = document.querySelector('.text-auto')
 
-function onClick() {
-    menuButton.addEventListener('click', function(){
-        navbar.classList.toggle('links-active')
-    });
-    
+let text = "a web developer";
+let index = 1;
+
+setInterval(writeText, 500)
+
+function writeText(){
+    text_auto.innerText = text.slice(0, index);
+    index++;
+    if(index > text.length){
+        index=1;
+    }
 }
-
-onClick();
